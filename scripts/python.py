496da9cdfefa1eaba9ebd363fc1412d604615377
Robin <robin.perice@protonmail.com>
2023-11-07 21:17:40 +0100

    Create python.py

---
+++ scripts/python.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import subprocess
+import argparse
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser("site")
+parser.add_argument("site", type=str)
+args = parser.parse_args()
+
+
+
+def clearUrl(url):
+  redirection=subprocess.check_output(["/Users/robinperice/Downloads/clean_url.sh", url])
+  return redirection.decode('utf-8')
+
+def replace(site):
+  f=open(site, 'r')
+  contents = f.read()
+  soup = BeautifulSoup(contents, features="html.parser")
+  for a in soup.findAll('a'):
+    mySite=clearUrl(a['href'])
+    a['href'] = mySite
+  f.close()
+  return soup
+
+def override(site, clearedHtml):
+  f=open(site, 'w')
+  f.truncate(0)
+  f.write(str(clearedHtml))
+  f.close()
+
+
+clearedHtml=replace(args.site)
+override(args.site, clearedHtml)
